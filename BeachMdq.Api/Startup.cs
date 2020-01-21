d270a22d294bd0d12bf3170d5dbe5f75740a32d8
naza1 <naza82@gmail.com>
2020-01-21 14:59:46 -0300

    Add swagger

--- BeachMdq.Api/Startup.cs
+++ BeachMdq.Api/Startup.cs
@@ -1,11 +1,11 @@
 using Infrastructure.Data;
-using Infrastructure.Data.Initializer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace BeachMdq.Api
 {
@@ -21,22 +21,26 @@ namespace BeachMdq.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<BeachMdqContext>(o => o.UseMySql(Configuration.GetConnectionString("BeachMdqDatabase")));
+
             services.AddControllers();
 
-            //services.AddTransient<IDbInitializer, UpdateByMigrationsDbInitializer>();
+            services.AddMvc();
 
-            AddEntityFramework(services);
-        }
-
-        public virtual void AddEntityFramework(IServiceCollection services)
-        {
-            var conn = Configuration.GetConnectionString("BeachMdqDatabase");
-
-            services.AddDbContextPool<BeachMdqContext>(o => o.UseMySql(conn));
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Beach Mdq API", 
+                    Version = "v1",
+                    Description = "A APi for Mdq Api"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbInitializer beachMdqDatabaseInitializer)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -52,6 +56,16 @@ namespace BeachMdq.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BeachMdq API V1");
             });
         }
     }
